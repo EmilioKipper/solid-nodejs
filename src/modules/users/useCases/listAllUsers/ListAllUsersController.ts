07c6b9e909008617db299d3dda99f3f6d98c06a3
Emílio Gularte Kipper <emilio.kipper@programmers.com.br>
2023-03-20 15:57:42 -0300

    Develop features and pass all tests

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,19 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const user_id_request = request.headers.user_id;
+
+    const user_id = Array.isArray(user_id_request)
+      ? user_id_request[0]
+      : user_id_request;
+
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.json(users);
+    } catch (error) {
+      return response.status(400).send({ error });
+    }
   }
 }
 
